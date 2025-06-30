d5ee33519152510b978890ecabdaf79a03d6f91d
ScipioJr <benedikt@puettbach.de>
2025-06-30 12:52:21 +0200

    Sensor für Ventilstatus und Fehlertoleranz

--- custom_components/ventilsteuerung/config_flow.py
+++ custom_components/ventilsteuerung/config_flow.py
@@ -1,1 +1,30 @@
-# config flow logic
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
+import homeassistant.helpers.config_validation as cv
+
+from .const import DOMAIN
+
+class VentilsteuerungConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+
+        schema = vol.Schema({
+            vol.Required(CONF_NAME): str,
+            vol.Required("ventil_switch"): cv.entity_id,
+            vol.Required("pumpe_switch"): cv.entity_id,
+            vol.Required("pumpe_user_switch"): cv.entity_id,
+            vol.Required("fahrzeit_oeffnen", default=180): int,
+            vol.Required("fahrzeit_schliessen", default=180): int,
+            vol.Required("grundstellung", default="geschlossen"): vol.In(["offen", "geschlossen"]),
+        })
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+            errors=errors
+        )
--- custom_components/ventilsteuerung/sensor.py
+++ custom_components/ventilsteuerung/sensor.py
@@ -1,1 +1,47 @@
-# sensor logic
+from homeassistant.helpers.entity import Entity
+from homeassistant.const import TIME_MINUTES
+from .const import DOMAIN
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    return
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    name = config_entry.data["name"]
+    timer_entity_id = f"timer.{name}_fahrzeit"
+    async_add_entities([VentilRestzeitSensor(hass, name, timer_entity_id)])
+
+class VentilRestzeitSensor(Entity):
+    def __init__(self, hass, name, timer_entity_id):
+        self._hass = hass
+        self._name = f"{name.capitalize()} Rest-Verfahrzeit"
+        self._entity_id = timer_entity_id
+        self._state = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        timer = self._hass.states.get(self._entity_id)
+        if timer and "remaining" in timer.attributes:
+            remaining = timer.attributes["remaining"]
+            if isinstance(remaining, str) and ":" in remaining:
+                return remaining
+        return "0:00"
+
+    @property
+    def unique_id(self):
+        return f"{self._entity_id}_restzeit"
+
+    @property
+    def unit_of_measurement(self):
+        return TIME_MINUTES
+
+    @property
+    def should_poll(self):
+        return True
+
+    @property
+    def device_class(self):
+        return "duration"
